1b7bf0622c0a8f3fc9f87e834dc05731490c61ef
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-11-08 14:38:13 +0100

    ho fatto stampare i dati nel modo richiesto

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let nome = prompt("Inserisci il nome");
 let cognome = prompt("Inserisci il cognome");
 let colore = prompt("Inserisci il tuo colore preferito");
+let numero = 21
+
+console.log(nome+cognome+colore+numero);
 
